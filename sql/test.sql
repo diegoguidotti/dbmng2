b22e1e1931f67e7ae9e550be9c90242d60590c22
guidotti <guidotti@MacBook-Air-di-guidotti.local>
2015-03-23 18:32:04 +0100

    fix sql script

--- sql/test.sql
+++ sql/test.sql
@@ -1,34 +1,31 @@
 CREATE TABLE `dbmng2`.`test` (
   `id` INTEGER  NOT NULL,
-  `name` VARCHAR(255)  NOT NULL,
+  `name` VARCHAR(255)   NULL,
   PRIMARY KEY (`id`)
 );
 ALTER TABLE `test` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
 
 
------------------------------------------------------------
--- add n_m tables - 2014-03-19
------------------------------------------------------------
 CREATE TABLE `dbmng2`.`test_father` (
   `id_father` INTEGER  NOT NULL AUTO_INCREMENT,
-  `check_field` INTEGER  NOT NULL,
-  `varchar_field` VARCHAR(255)  NOT NULL,
-  `select_field` INTEGER  NOT NULL,
+  `check_field` INTEGER   NULL,
+  `varchar_field` VARCHAR(255)   NULL,
+  `select_field` INTEGER   NULL,
   PRIMARY KEY (`id_father`)
 );
 
 CREATE TABLE `dbmng2`.`test_child` (
   `id_child` INTEGER  NOT NULL AUTO_INCREMENT,
-  `child_name` VARCHAR(255)  NOT NULL,
+  `child_name` VARCHAR(255)   NULL,
   PRIMARY KEY (`id_child`)
 );
 
 CREATE TABLE `dbmng2`.`test_father_child` (
   `id_father_child` INTEGER  NOT NULL AUTO_INCREMENT,
-  `id_father` INTEGER  NOT NULL,
-  `id_child` INTEGER  NOT NULL,
+  `id_father` INTEGER   NULL,
+  `id_child` INTEGER   NULL,
   PRIMARY KEY (`id_father_child`)
 );
-ALTER TABLE `dbmng2`.`test_father` ADD COLUMN `date_field` DATE  NOT NULL AFTER `select_field`;
+ALTER TABLE `dbmng2`.`test_father` ADD COLUMN `date_field` DATE   NULL AFTER `select_field`;
 ALTER TABLE `dbmng2`.`test_father` MODIFY COLUMN `date_field` DATE  DEFAULT NULL;
 
