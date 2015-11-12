c3088a16d68c11d36d6333777da6d77e805e955a
diegoguidotti <diego.guidotti@gmail.com>
2015-11-12 17:01:15 +0100

    Grunt

---
+++ js/Gruntfile.js
@@ -0,0 +1,104 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    concat_sourcemap: {
+      options: {
+        sourcesContent: true
+      },
+      target: {
+        files: { 
+          'dist/dbmng.js': [
+            
+            // License & version info, start the containing closure
+            'src/Intro.js',
+            
+            // Simple inheritance
+            'src/Class.js',
+            // IE9 polyfills
+            'src/Ie9.js',
+            // Utils like extend, each, and trigger
+            'src/Utilities.js',
+            'src/Dbmng.js',
+            'src/themes/*.js',
+            'src/widgets/*.js',
+            
+            // End the closure
+            'src/Outro.js'
+          ],
+        }
+      }
+    },
+    removelogging : {
+      main : {
+        src : 'dist/dbmng.js',
+        dest : 'dist/dbmng.built.js'        
+      }
+    },
+    uglify: {
+      dist: {
+        src: 'dist/dbmng.built.js',
+        dest: 'dist/dbmng.min.js'
+      },
+      options: {
+        preserveComments: 'some'
+      }
+    },
+    watch: {
+      scripts: {
+        files: ["src/**/*.js"],
+        tasks: ["concat_sourcemap"]
+      }
+    },
+    jshint: {
+      options: {
+        browser: true,
+        indent: 2,
+        nonbsp: true,
+        nonew: true,
+        immed: true,
+        latedef: true,
+        globals: {
+          'jQuery': true
+        },
+      },      
+      beforeconcat: [
+         // Simple inheritance
+            'src/Class.js',
+            // IE9 polyfills
+            'src/Ie9.js',
+            // Utils like extend, each, and trigger
+            'src/Utilities.js',
+            'src/Dbmng.js.js',
+
+            'src/themes/*.js',
+            'src/widgets/*.js',        
+      ],
+      afterconcat: {
+        options: {
+          undef: true
+        },
+      globals: {
+        jQuery: true
+      },
+        files: {
+          src: ['dist/dbmng.built.js']
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-concat-sourcemap');
+  grunt.loadNpmTasks("grunt-remove-logging");
+
+
+
+  // Default task.
+  //grunt.registerTask('default', ['jshint:beforeconcat','concat_sourcemap','removelogging','uglify']);
+  grunt.registerTask('default', ['jshint:beforeconcat','concat_sourcemap','removelogging','jshint:afterconcat','uglify']);
+
+};
