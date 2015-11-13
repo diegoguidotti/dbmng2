23e431fbc6307440daa11c14a80d88daa0fe2e0c
Michele <mamminim@gmail.com>
2015-11-13 16:28:30 +0100

    fine tuning

--- js/src/widgets/AbstractWidget.js
+++ js/src/widgets/AbstractWidget.js
@@ -8,14 +8,49 @@
 // Michele Mammini
 /////////////////////////////////////////////////////////////////////
 
-Dbmng.AbstractWidget = Class.extend({  
-  createWidget: function(options){
-      var aField=options.aField;
-      var theme =options.theme;         
-      var input={'value':this.getDefaultValue(), 'label':aField.label};
-      return theme.getFormInput(input);
+Dbmng.AbstractWidget = Class.extend({
+  createWidget: function( options ) {
+    // var aField = options.aField;
+    var theme  = options.theme;
+    
+    options.aField.value = this.getFieldValue(options);
+    
+    return theme.getInput(options.aField);
   },
-  getDefaultValue: function(options){
-    return 'vuoto';        
-  }      
+  
+  createField: function( options ) {
+    var theme;
+    if( options.theme ) {
+      theme = options.theme;
+    }
+    else {
+      theme = new Dbmng.AbstractTheme();
+      options.theme  = theme;
+    }
+    
+    options.aField.field = options.field;
+    
+    var el = theme.getFieldContainer(options.aField);
+    el.appendChild(theme.getLabel(options.aField));
+    el.appendChild(this.createWidget(options));
+    return el;
+  },
+  
+  getDefaultValue: function( options ) {
+    return '';
+  },
+  
+  getFieldValue: function( options ) {
+    var v;
+    if(options.value){
+      v = options.value;
+    }
+    else if( options.aField.default ) {
+      v = options.aField.default;
+    }
+    else{
+      v = this.getDefaultValue();
+    }
+    return v;
+  }
 });
