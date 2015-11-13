0fe5189b1e02f9d1bee81500c80533ac8cc7e8c9
Michele <mamminim@gmail.com>
2015-11-13 16:27:59 +0100

    Developed new Class SelectWidget

---
+++ js/src/widgets/SelectWidget.js
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////////////////////
+// AbstractWidget
+// 12 November 2015
+// 
+//
+// Developed by :
+// Diego Guidotti
+// Michele Mammini
+/////////////////////////////////////////////////////////////////////
+
+Dbmng.SelectWidget = Dbmng.AbstractWidget.extend({
+  createWidget: function(options){
+    var aField=options.aField;
+    var theme =options.theme;
+    
+    aField.value = this.getFieldValue(options);
+    aField.field = options.field;
+    
+    return theme.getSelect(aField);
+  }
+});
