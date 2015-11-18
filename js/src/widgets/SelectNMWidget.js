9e978fc2be10fc6ee26af0517c5487d70ef344b5
Michele <mamminim@gmail.com>
2015-11-18 16:37:34 +0100

    managed out_type (select or checkbox)

--- js/src/widgets/SelectNMWidget.js
+++ js/src/widgets/SelectNMWidget.js
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////
 // AbstractWidget
-// 12 November 2015
+// 18 November 2015
 // 
 //
 // Developed by :
@@ -12,22 +12,41 @@ Dbmng.SelectNMWidget = Dbmng.AbstractWidget.extend({
   createWidget: function(){
     this.aField.value = this.getFieldValue();
     this.aField.field = this.field;
-    
+
     return this.theme.getSelectNM(this.aField);
   },
-  
+
   getValue: function(){
     var aVal, aRet;
-    if( this.aField.type == 'int' ) {
-      aVal = jQuery(this.widget).val();
-      
-      aRet = [];
-      aVal.forEach(function(entry) {
-        aRet.push(parseInt(entry));
-      });
+
+    var out_type = "select";
+    if( this.aField.out_type == 'checkbox' ) {
+      out_type = "checkbox";
     }
-    else {
-      aRet = jQuery(this.widget).val();
+
+    if( out_type == "select" ) {
+      if( this.aField.type == 'int' ) {
+        aVal = [].concat(jQuery(this.widget).val());
+
+        aRet = [];
+        aVal.forEach(function(entry) {
+          aRet.push(parseInt(entry));
+        });
+      }
+      else {
+        aRet = jQuery(this.widget).val();
+      }
+    }
+    else if( out_type == "checkbox" ) {
+      var cb = jQuery(this.widget).children('li').children();
+      aVal = [];
+      cb.each(function(k,v){
+        if( v.checked ) {
+          console.log(v.value);
+          aVal.push(parseInt(v.value));
+        }
+      });
+      aRet = aVal;
     }
     return aRet;
   }
