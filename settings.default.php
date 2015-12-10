1229bbda261642b5d8938f9c5d4a5064fb81ec09
diegoguidotti <diego.guidotti@gmail.com>
2015-12-10 19:44:58 +0100

    fix Travis (cp configs)

--- settings.default.php
+++ settings.default.php
@@ -2,8 +2,8 @@
 
 	
 	$aSetting=Array();	
-	$aSetting['DB']['DB_DSN']="mysql:user=root;host=127.0.0.1;dbname=...;password=...";
-	$aSetting['DB']['DB_USER']="...";
+	$aSetting['DB']['DB_DSN']="mysql:user=root;host=127.0.0.1;dbname=root;password=";
+	$aSetting['DB']['DB_USER']="root";
 	$aSetting['DB']['DB_PASSWD']="...";
 
 ?>
