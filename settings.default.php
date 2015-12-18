63d4d2dc7fde0aa6d72b1e6924e91a231dfce21b
Diego Guidotti <diego.guidotti@gmail.com>
2015-12-18 16:38:12 +0100

    Update settings.default.php

--- settings.default.php
+++ settings.default.php
@@ -2,7 +2,7 @@
 
 	
 	$aSetting=Array();	
-	$aSetting['DB']['DB_DSN']="mysql:user=root;host=127.0.0.1;dbname=root;password=";
+	$aSetting['DB']['DB_DSN']="mysql:user=root;host=127.0.0.1;dbname=dbmng2;password=";
 	$aSetting['DB']['DB_USER']="root";
 	$aSetting['DB']['DB_PASSWD']="...";
 
