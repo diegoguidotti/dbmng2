65209125547198fc5308f2a465e7e8d2a0046cc2
diegoguidotti <diego.guidotti@gmail.com>
2016-01-12 17:50:11 +0100

    Fix dateWidget

--- js/src/widgets/DateWidget.js
+++ js/src/widgets/DateWidget.js
@@ -11,33 +11,57 @@
 Dbmng.DateWidget = Dbmng.AbstractWidget.extend({
   createWidget: function(){
     this.aField.value = this.getFieldValue();
-    this.aField.field_type='text';
+    if(typeof jQuery.datepicker !== 'undefined'){
+      this.aField.field_type='hidden';
+    }
+    else{
+      this.aField.field_type='date';
+    }
     return this.theme.getInput(this.aField);
   },
   createField: function(data_val){
+
     var self=this;
+    var date_format_view='dd-mm-yy';
+    console.log(self);
+    if(self.aField.date_format_view){
+      date_format_view=self.aField.date_format_view;
+      console.log(date_format_view);
+    }
+
     var el = this._super(data_val);
-    
-    var aVField = {};
-    aVField.field = this.aField.field + '_view';
-    
-    var elv = this.theme.getInput(aVField);
-    
-    jQuery(elv).blur(function(){
-      console.log(jQuery(elv).val());
-      if( jQuery(elv).val() == '' ) {
-        self.widget.value = "";
+    if(typeof jQuery.datepicker !== 'undefined'){
+      var aVField = {};
+      aVField.field = this.aField.field + '_view';
+
+      var elv = this.theme.getInput(aVField);
+      if(typeof data_val !== undefined && data_val!=='' && data_val !==null ){
+        elv.value=jQuery.datepicker.formatDate(date_format_view,jQuery.datepicker.parseDate( "yy-mm-dd", data_val ));
       }
-      
-    });
-    jQuery(elv).datepicker({altField: el, dateFormat:'dd-mm-yy' , altFormat:'yy-mm-dd'});  
-    
-    
-  //$html .='<script>  jQuery("#dbmng_'.$fld.'_tmp").blur(function(){ if(jQuery("#dbmng_'.$fld.'_tmp").val()==""){  jQuery("#dbmng_'.$fld.'").val("");} }); jQuery(function() { jQuery( "#dbmng_'.$fld.'_tmp" ).datepicker({altField: \'#dbmng_'.$fld.'\', dateFormat:\'dd-mm-yy\' , altFormat: \'yy-mm-dd\'});  });  </script>';
-    
-    
-    el.appendChild(elv);
-    
+
+      jQuery(elv).blur(function(){
+        console.log(jQuery(elv).val());
+        if( jQuery(elv).val() === '' ) {
+          self.widget.value = "";
+        }
+
+      });
+      jQuery(elv).datepicker({altField: jQuery(self.widget),
+        dateFormat:date_format_view , altFormat:'yy-mm-dd'
+      });
+
+      el.appendChild(elv);
+    }
+
     return el;
-  }
+  },
+  getValue: function() {
+    var el = this._super();
+		if(el===''){
+			return null;
+		}
+		else{
+				return el;
+			}
+		}
 });
