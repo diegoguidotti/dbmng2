d2938091819097bd695583143b6c3742e23fb0ad
diegoguidotti <diego.guidotti@gmail.com>
2016-02-01 12:32:45 +0100

    Drupal Modules

---
+++ src/drupal/mod_dbmng2/mod_dbmng2.js
@@ -0,0 +1,138 @@
+Dbmng.defaults.aParam = {};
+Dbmng.defaults.aParam.ui = {};
+Dbmng.defaults.aParam.ui.btn_edit = {label:'Edit', icon:'fa fa-pencil'};
+Dbmng.defaults.aParam.ui.btn_edit_inline = {label:'Edit inline', icon:'fa fa-pencil-square-o'};
+Dbmng.defaults.aParam.ui.btn_delete = {label:'Delete', icon:'fa fa-trash', confirm_message: 'Are you sure to delete the table?'};
+Dbmng.defaults.aParam.ui.btn_insert = {label:'Insert new tables', class:'btn-success btn-block'};
+Dbmng.defaults.aParam.ui.btn_save = {label:'Save table', class:'btn-success btn-block'};
+Dbmng.defaults.aParam.ui.btn_cancel = {label:'Cancel', class:'btn-danger btn-block'};
+Dbmng.defaults.aParam.ui.table_class = 'table-condensed table-hover';
+Dbmng.defaults.aParam.user_function = {upd:1, del:1, ins:1};
+
+function dbmng2_show_tables() {
+  var div_id = 'dbmng2_table_list';
+  var path   = base_path + 'dbmng2/rest/';
+  var table  = 'dbmng_tables';
+  var field  = 'dbmng_fields';
+  var aParam = {};
+  
+  jQuery('#dbmng2_table_edit').hide();
+  jQuery('#dbmng2_table_list').show();
+  
+  var template="";
+  template += "<div>";
+    template += "<h3>Dbmng Table</h3>";
+    template += "<table class='table'>";
+      template += "<tr><td data-content='id_table' class='col-xs-6'></td><td data-content='table_name' class='col-xs-6'></tr>";
+      template += "<tr><td data-content='table_label' class='col-xs-6'></td><td data-content='table_alias' class='col-xs-6'></tr>";
+      template += "<tr><td colspan='2' data-content='table_desc' class='col-xs-12'></td></tr>";
+      template += "<tr><td colspan='2' data-content='param' class='col-xs-12'></td></tr>";
+    template += "</table>";
+  template += "</div>";
+  aParam.template_form = template;
+  
+  aParam.custom_function = {action: 'dbmng2_show_fields', label: 'Show fields', icon: 'fa fa-th-list'};
+
+  var theme_boot = new Dbmng.BootstrapTheme();
+  var crud = new Dbmng.Crud(
+    {
+      aParam:aParam, theme:theme_boot, url: path + 'api/' + table,
+      success:function(self){
+        console.log(self);
+        self.createTable({div_id:'#'+div_id});
+      },
+      crud_success: function(method, data){
+        if( method == 'insert' ) {
+          if( data.ok ) {
+            jQuery.ajax({ 
+              type: 'POST', 
+              url: "dbmng2/ajax?fill_dbmng_fields=on", 
+              data: {id_table:data.inserted_id},
+              success: function(msg){ 
+                obj = JSON.parse(msg);
+                if( obj.ok ) {
+                  dbmng2_show_fields(obj.id_table);
+                }
+              } 
+            }); 
+          }
+        }
+        else if( method == 'delete' ) {
+          jQuery.ajax({ 
+            type: 'POST', 
+            url: "dbmng2/ajax?delete_dbmng_fields=on", 
+            data: {id_table:data.deleted_id},
+            success: function(msg){ 
+              obj = JSON.parse(msg);
+              if( obj.ok ) {
+                console.log(obj);
+              }
+            } 
+          }); 
+        }
+      }
+    }
+  );
+}
+
+function dbmng2_show_fields(id_table) {
+  var div_id = 'dbmng2_table_edit';
+  var path   = base_path + 'dbmng2/rest/';
+  var table  = 'dbmng_tables';
+  var field  = 'dbmng_fields';
+  var aParam = {};
+  
+  jQuery('#dbmng2_table_list').hide();
+  jQuery('#dbmng2_table_edit').show();
+  
+  var template="";
+  template += "<div>";
+    template += "<h3>Dbmng Fields</h3>";
+    template += "<table class='table'>";
+      template += "<tr><td data-content='id_field' class='col-xs-3'></td><td data-content='field_name' class='col-xs-3'></td><td data-content='id_field_type' class='col-xs-3'></td><td data-content='pk' class='col-xs-3'></td></tr>";
+      template += "<tr><td data-content='field_label'></td><td data-content='field_widget'></td><td data-content='nullable'></td><td data-content='field_size'></td></tr>";
+      template += "<tr><td data-content='field_order'></td><td data-content='readonly'></td><td data-content='skip_in_tbl'></td><td data-content='is_searchable'></td></tr>";
+      
+      template += "<tr><td colspan='4' data-content='voc_sql'></td></tr>";
+      template += "<tr><td colspan='4' data-content='param'></td></tr>";
+      template += "<tr><td colspan='2' data-content='field_note'></td><td data-content='default_value'></td><td data-content='field_label_long'></td></tr>";
+    template += "</table>";
+  template += "</div>";
+  aParam.template_form = template;
+
+  aParam.ui = {};
+  aParam.ui.btn_insert = {label:'Insert new fields'};
+  aParam.ui.btn_save = {label:'Save field'};
+  aParam.ui.btn_delete = {confirm_message: 'Are you sure to delete the field?'};
+  aParam.search = {id_table:id_table};
+  
+  var theme_boot = new Dbmng.BootstrapTheme();
+  var crud = new Dbmng.Crud(
+    {
+      aParam:aParam, theme:theme_boot, url: path + 'api/' + field,
+      success:function(self){
+        console.log(self);
+        self.createTable({div_id:'#'+div_id});
+      }
+    }
+  );
+  
+  var api = new Dbmng.Api({url:path + 'api/' + table});
+  api.select({
+    search: "id_table="+id_table,
+    success: function(data){
+      jQuery(document).ajaxStop(function(){
+        jQuery('#dbmng2_table_edit').prepend("Tablename: <strong>"+data.data[0].table_name+"</strong> [id="+id_table+"]");
+        
+        jQuery('.dbmng_form_button_right').html('<button id="back" class="btn-block btn btn-default">Back to table</button>');
+        
+        jQuery('#back').click(function(){
+          alert("back");
+          jQuery('#dbmng2_table_edit').hide();
+          jQuery('#dbmng2_table_list').show();
+        });
+      });
+    }
+  });
+  
+}
---
+++ src/drupal/mod_dbmng2/mod_dbmng2.php
@@ -0,0 +1,87 @@
+<?php
+use Dbmng\Db;
+use Dbmng\Dbmng;
+use Dbmng\Api;
+use Dbmng\App;
+use Dbmng\Login;
+use Respect\Rest\Router;
+use Dbmng\DbmngHelper;
+use Dbmng\Util;
+
+function dbmng2_rest_response()
+{
+  global $user;
+  global $DBMNG;
+  global $base_path;
+  
+  $path = $base_path . "dbmng2/rest";
+  $router = new \Respect\Rest\Router($path);
+  
+  $app = new App($DBMNG, array('user'=>(array)$user, 'aParamDefault' => array('dbname' => 'registro')));
+  $h = new DbmngHelper($app);
+  $h->exeAllRest( $router );
+}
+
+function dbmng2_ajax_response()
+{
+  global $user;
+  global $DBMNG;
+  
+  $app = new App($DBMNG, array('user'=>(array)$user, 'aParamDefault' => array('dbname' => 'registro')));
+  $h = new DbmngHelper($app);
+
+  if( isset($_REQUEST['fill_dbmng_fields']) )
+    {
+      $id_table = $_REQUEST['id_table'];
+      $res = $h->getTableStructure($id_table);
+      
+      $result = json_encode($res);
+    }
+  elseif( isset($_REQUEST['delete_dbmng_fields']) )
+    {
+      if( isset($_REQUEST['id_table']) )
+        {
+          $sql = "delete from dbmng_fields where id_table = :id_table";
+          $var = array(':id_table' => $_REQUEST['id_table']);
+          $res = $DBMNG->delete($sql, $var);
+        }
+      else
+        {
+          $res['ok'] = false;
+          $res['message'] = "Something went wrong.";
+        }
+        
+        $result = json_encode($res);
+    }
+  echo $result;
+}
+
+function dbmng2_manager() 
+{
+  drupal_add_js('https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.js', array('cache' => false));
+  drupal_add_js('https://cdnjs.cloudflare.com/ajax/libs/blueimp-file-upload/9.5.7/jquery.iframe-transport.min.js', array('cache' => false));
+  drupal_add_js('https://cdnjs.cloudflare.com/ajax/libs/blueimp-file-upload/9.5.7/jquery.fileupload.min.js', array('cache' => false));
+  drupal_add_css( "https://cdnjs.cloudflare.com/ajax/libs/blueimp-file-upload/9.5.7/css/jquery.fileupload.min.css", "external" );
+  
+  drupal_add_js('sites/all/libraries/dbmng2/js/dist/dbmng.js',array('cache' => false));
+  
+  drupal_add_js('sites/all/modules/mod_dbmng2/mod_dbmng2.js',array('cache' => false));
+  drupal_add_css('sites/all/modules/mod_dbmng2/mod_dbmng2.css',array('cache' => false));
+  
+  drupal_add_js('sites/all/libraries/rifiuti/typeahead.bundle.min.js',array('cache' => false));
+  drupal_add_js('sites/all/libraries/rifiuti/hendlebars.js',array('cache' => false));
+  
+  global $DBMNG;
+  global $user;
+  global $base_path;
+  
+  drupal_add_js("var base_path=".$base_path, "inline");
+  drupal_add_js("jQuery(document).ready(function(){dbmng2_show_tables()})", "inline");
+  
+  $html = "";
+  
+  $html .= "<div id='dbmng2_table_list'></div>";
+  $html .= "<div id='dbmng2_table_edit'></div>";
+  return $html;
+}
+
