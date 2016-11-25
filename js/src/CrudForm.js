b69aa78882f13bd3308e203092c758cb524bfecf
Michele <mamminim@gmail.com>
2016-11-25 14:47:36 +0100

    CrudForm

--- js/src/CrudForm.js
+++ js/src/CrudForm.js
@@ -13,7 +13,8 @@ Dbmng.CrudForm = Class.extend({
     this.ready=true;
     this.div_id = options.div_id;
     this.aParam = jQuery.extend(true, {}, Dbmng.defaults.aParam, options.aParam);
-    
+    this.form_ready = options.form_ready;
+
     if( options.theme ) {
       this.theme = options.theme;
     }
@@ -91,13 +92,10 @@ Dbmng.CrudForm = Class.extend({
   },
   
   createForm: function( id ){
-    console.log("createForm");
-    console.log(id);
     if( this.ready ) {
-      console.log(this.aForm);
       var type='update';
       if(typeof id==='undefined'){
-          type='insert';
+        type='insert';
       }
       var self = this;
       var key = this.aForm.primary_key[0];
@@ -106,55 +104,78 @@ Dbmng.CrudForm = Class.extend({
         this.api.select({
           search:key+"="+id,
           success: function(data){
-            jQuery('#'+self.div_id).html("");
-            console.log(data);
-            jQuery('#'+self.div_id).append(self.form.createForm(data.data[0]));
-            
-            // Copiata da Crud.createForm !!!
-            var label_save=self.aParam.ui.btn_save.label;
-            var opt_save=self.aParam.ui.btn_save;
-            var label_cancel='Cancel';
-            var opt_cancel={};
-
-            if(self.aParam.ui.btn_cancel){
-              label_cancel=self.aParam.ui.btn_cancel.label;
-              opt_cancel=self.aParam.ui.btn_cancel;
-            }
-
-            var button = self.theme.getButton(label_save, opt_save);
-            jQuery(button).click(function(){
-              var valid=self.form.isValid();
-              if(valid.ok===false){
-                jQuery('#'+self.div_id).find(".dbmng_form_button_message").html(valid.message);
-              }
-              else if(type=='update'){
-                self.api.update({key:key,data:self.form.getValue(),success:function(data){
-
-                  if(!data.ok){
-
-                    var msg=self.theme.alertMessage(data.message);
-                    jQuery('#'+self.div_id).find(".dbmng_form_button_message").html(msg);
-
-                  }
-                  else{
-                    self.createForm(id);
-                  }
-                }});
-              }
-            });
-
-            var button_cancel = self.theme.getButton(label_cancel, opt_cancel);
-            jQuery(button_cancel).click(function(){
-              jQuery('#'+self.div_id).html('');
-              self.createTable({div_id:self.div_id});
-            });
-            jQuery('#'+self.div_id).append("<div id='dbmng_buttons_row' class='row' style='margin-top: 20px;margin-bottom: 100px;'><div class='dbmng_form_button_message col-xs-12'></div><div class='dbmng_form_button_left col-xs-4'></div><div class='col-xs-4'></div><div class='dbmng_form_button_right col-xs-4'></div></div>");
-            jQuery('#'+self.div_id).find('.dbmng_form_button_left').append(button_cancel);
-            jQuery('#'+self.div_id).find('.dbmng_form_button_right').append(button);
+            self.generateForm(type,key,id,data);
           }
         });
       }
-  
+      else if( type == 'insert' ) {
+        self.generateForm(type,key);
+      }
     }
+  },
+
+  generateForm: function( type, key, id, data ) {
+    var self = this;
+    jQuery('#'+self.div_id).html("");
+    console.log(data);
+    if( typeof data !== 'undefined' ) {
+      jQuery('#'+self.div_id).append(self.form.createForm(data.data[0],self.aParam.template_form));
+    }
+    else {
+      jQuery('#'+self.div_id).append(self.form.createForm());
+    }
+    
+    if(typeof self.form_ready=='function'){
+      self.form_ready(type, this.form);
+    }
+    
+    // Copiata e modificata a partire da Crud.createForm !!!
+    var label_save=self.aParam.ui.btn_save.label;
+    var opt_save=self.aParam.ui.btn_save;
+    var label_cancel='Cancel';
+    var opt_cancel={};
+
+    if(self.aParam.ui.btn_cancel){
+      label_cancel=self.aParam.ui.btn_cancel.label;
+      opt_cancel=self.aParam.ui.btn_cancel;
+    }
+
+    var button = self.theme.getButton(label_save, opt_save);
+    jQuery(button).click(function(){
+      var valid=self.form.isValid();
+      if(valid.ok===false){
+        jQuery('#'+self.div_id).find(".dbmng_form_button_message").html(valid.message);
+      }
+      else if(type=='update'){
+        self.api.update({key:key,data:self.form.getValue(),success:function(data){
+          if(!data.ok){
+            var msg=self.theme.alertMessage(data.message);
+            jQuery('#'+self.div_id).find(".dbmng_form_button_message").html(msg);
+          }
+          else{
+            self.createForm(id);
+          }
+        }});
+      }
+      else if(type=='insert'){
+        self.api.insert({data:self.form.getValue(),success:function(data){
+          console.log(data);
+          if(typeof self.crud_success=='function'){
+            self.crud_success('insert', data);
+          }
+          jQuery(self.div_id).html('');
+          self.createForm(data.inserted_id);
+        }});
+      }
+    });
+
+    var button_cancel = self.theme.getButton(label_cancel, opt_cancel);
+    jQuery(button_cancel).click(function(){
+      jQuery('#'+self.div_id).html('');
+      //self.createTable({div_id:self.div_id});
+    });
+    jQuery('#'+self.div_id).append("<div id='dbmng_buttons_row' class='row' style='margin-top: 20px;margin-bottom: 100px;'><div class='dbmng_form_button_message col-xs-12'></div><div class='dbmng_form_button_left col-xs-4'></div><div class='col-xs-4'></div><div class='dbmng_form_button_right col-xs-4'></div></div>");
+    jQuery('#'+self.div_id).find('.dbmng_form_button_left').append(button_cancel);
+    jQuery('#'+self.div_id).find('.dbmng_form_button_right').append(button);
   }
 });
