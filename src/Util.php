d07ec3229497d214046a1fcbb8bc837119357dbe
diegoguidotti <diego.guidotti@gmail.com>
2017-03-06 18:27:13 +0100

    test

--- src/Util.php
+++ src/Util.php
@@ -47,20 +47,20 @@ class Util
 			return $defVal;
 		}
 
-	}	
+	}
 
 	/////////////////////////////////////////////////////////////////////////////
 	// print an array (with <pre> html tags)
 	// ======================
 	/**
 	\param $array  		  array to be printed
-	\return $ret			  the html version of the array 
+	\return $ret			  the html version of the array
 	*/
 	static function print_r($array)
 	{
 		return "<pre class='dbmng_print_r'>".print_r($array,true)."<pre>";
-	}	
-	
+	}
+
 	static function toArray($obj)
   {
     if (is_object($obj)) $obj = (array)$obj;
@@ -75,4 +75,16 @@ class Util
 
     return $new;
   }
+
+
+  static function template_var($txt, $array){
+
+    foreach($array as $k=>$v){
+      // echo $k;
+      // echo $v;
+        $txt=str_replace("{{".$k."}}",$v,$txt);
+    }
+    return $txt;
+  }
+
 }
--- tests/UtilTest.php
+++ tests/UtilTest.php
@@ -11,9 +11,9 @@ class UtilTest extends \PHPUnit_Framework_TestCase
 	public function test_req_equal()
 	{
 		$aTest = array('a' => 1, 'b' => 2);
-		$this->assertEquals(true,Util::var_equal($aTest, 'a', 1));	
-		$this->assertEquals(false,Util::var_equal($aTest, 'a', 2));	
-		$this->assertEquals(false,Util::var_equal($aTest, 'c', 1));	
+		$this->assertEquals(true,Util::var_equal($aTest, 'a', 1));
+		$this->assertEquals(false,Util::var_equal($aTest, 'a', 2));
+		$this->assertEquals(false,Util::var_equal($aTest, 'c', 1));
 	}
 
 
@@ -22,8 +22,33 @@ class UtilTest extends \PHPUnit_Framework_TestCase
 
 		$aTest = array('a' => 1, 'b' => 2);
 
-		$this->assertEquals(1,Util::get_val($aTest, 'a', 99));	
-		$this->assertEquals(99,Util::get_val($aTest, 'c', 99));	
+		$this->assertEquals(1,Util::get_val($aTest, 'a', 99));
+		$this->assertEquals(99,Util::get_val($aTest, 'c', 99));
 	}
-	
+
+
+  public function test_template_var()
+  {
+        $txt="Ciao sono {{name}} io";
+        $array=Array("name"=>"Diego");
+        $this->assertEquals("Ciao sono Diego io",Util::template_var($txt,$array));
+
+        $txt="Ciao sono {{name}} {{surname}} io";
+        $array=Array("name"=>"Diego","surname"=>"Guidotti");
+        $this->assertEquals("Ciao sono Diego Guidotti io",Util::template_var($txt,$array));
+
+        $txt="Ciao sono {{name}} {{surname}} io";
+        $array=Array("name"=>"Diego","surname"=>"Guidotti","age"=>"12");
+        $this->assertEquals("Ciao sono Diego Guidotti io",Util::template_var($txt,$array));
+
+        // Util::template_var($sql_var, JOIN(_$REQUEST,$aParam['filters']);
+
+        // " WHERE id_azienda in(select id_azienda from aziedne WHERE uid={{system_uid}}) AND  uid={{filter_uid}} AND id_azienda={{request_id_azienda}} ";
+        //
+        // 'filter_uid' => $aParam['filters']['uid']
+        // 'request_id_azienda' => $_REQUEST['id_azienda']
+
+
+  }
+
 }
