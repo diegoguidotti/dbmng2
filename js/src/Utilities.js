2a19e30cfbc199225c9fd0c23e98c15e64dd379d
Michele <mamminim@gmail.com>
2017-03-13 16:01:02 +0100

    startsWidth

--- js/src/Utilities.js
+++ js/src/Utilities.js
@@ -15,3 +15,16 @@ function exeExternalFunction (fnstring, params) {
       return false;
   }
 }
+
+if (typeof String.prototype.startsWith != 'function') {
+  // see below for better implementation!
+  String.prototype.startsWith = function (str){
+    return this.indexOf(str) == 0;
+  };
+}
+
+if (typeof String.prototype.endsWith != 'function') {
+	String.prototype.endsWith = function(suffix) {
+		  return this.indexOf(suffix, this.length - suffix.length) !== -1;
+	};
+}
