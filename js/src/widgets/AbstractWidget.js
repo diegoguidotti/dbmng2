433fdd98ccbe5d3781c894182482176f8a8a0e41
Michele <mamminim@gmail.com>
2017-06-21 16:28:45 +0200

    fixed isValid() method

--- js/src/widgets/AbstractWidget.js
+++ js/src/widgets/AbstractWidget.js
@@ -195,10 +195,11 @@ Dbmng.AbstractWidget = Class.extend({
 
     if( this.toValidate(nullable) ) { //( nullable === 0 && this.aField.field_type != 'hidden' && this.aField.key != 1 ) {
       validated = true;
-      if( this.getValue()===null || this.getValue()==='' ) {
+
+      if( this.getValue()===null || this.getValue()==='' || isNaN(this.getValue()) ) {
         console.log(this.aField);
-          ok=false;
-          message='Empty Field ['+this.aField.field+']';
+        ok=false;
+        message='Empty Field ['+this.aField.field+']';
       }
     }
 
