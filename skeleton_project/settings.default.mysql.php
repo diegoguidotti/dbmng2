cd5caaaecaa4e4ff98ebbfb6e9ce0b5b3ce57c41
klean97 <kleanhoxha@yahoo.it>
2017-10-03 13:51:46 +0200

    default settings update

--- skeleton_project/settings.default.mysql.php
+++ skeleton_project/settings.default.mysql.php
@@ -1,12 +1,16 @@
 <?php
 
-	
+
+	define( 'DBMNG2_API_PATH', 'api/dbmng' );
+
 	$aSetting=Array();
 
-  $aSetting['BASE_PATH']="/xxx_project_name";
+	$aSetting['BASE_PATH']="/xxx_project_name";
 
-  $aSetting['DB']['DEBUG'] = true;
-	
+	$aSetting['DB']['DEBUG'] = true;
+
+	$aSetting['API_PREFIX']="/";
+
 	$aSetting['DB']['DB_DSN']="mysql:user=xxx_user;host=127.0.0.1;dbname=xxx_dbname;password=xxx_password";
 	$aSetting['DB']['DB_USER']="xxx_user";
 	$aSetting['DB']['DB_PASSWD']="xxx_password";
