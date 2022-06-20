c529f7347f2b198389be547b2ed21b1af07ed483
Michele Mammini <mamminim@gmail.com>
2022-06-20 11:57:54 +0200

    new dbmng

--- js/dist/dbmng.js
+++ js/dist/dbmng.js
@@ -337,15 +337,18 @@ geo
     }
 
     if(normal){
+
+      //get an empty form
       form = this.theme.getForm();
 
+      //the form can be single or with a children; use the children if exists
+      var appendTo=form;
+      if(form.firstChild){
+        appendTo=form.firstChild;
+      }
+
   		for(var key in fields){
-        if(form.firstChild){
-          form.firstChild.appendChild(fields[key]);
-        }
-        else{
-          form.appendChild(fields[key]);
-        }
+        appendTo.appendChild(fields[key]);
   		}
     }
     else{
@@ -823,7 +826,12 @@ Dbmng.FormInline = Class.extend({
 
         if(self.aParam.do_delete){
           var col=jQuery('<td></td>').appendTo(r);
-          var button_delete=jQuery('<button data-record="'+current_record+'">Del</button>')[0];
+          var del_label='Del';
+          if (self.aParam.del_label) {
+            del_label=self.aParam.del_label;
+          }
+          // debugger
+          var button_delete=jQuery('<button data-record="'+current_record+'">'+del_label+'</button>')[0];
           col[0].appendChild(button_delete);
 
           jQuery(button_delete).click(function(){
@@ -2934,6 +2942,18 @@ Dbmng.AbstractTheme = Class.extend({
   getDeleteButton: function(label){
     var el = this.getButton("X",{type:'span'});
     return el;
+  },
+  setErrorState: function(element, ok, message){
+    var par=jQuery(jQuery(element.widget).parents('.dbmng_form_row')[0]);
+    par.find('span.error_message').remove();
+
+    if(ok){
+      par.removeClass('alert-danger').addClass('alert-success');
+    }
+    else{
+      par.append('<span class="error_message">'+message+'</span>');
+      par.removeClass('alert-success').addClass('alert-danger');
+    }  
   }
 
 
@@ -3000,6 +3020,272 @@ Dbmng.AppTheme = Dbmng.AbstractTheme.extend({
     this.addTitle(icn, label);
     return icn;
   }
+
+});
+
+/////////////////////////////////////////////////////////////////////
+// BootstrapTheme
+// 18 November 2015
+//
+//
+// Developed by :
+// Diego Guidotti
+// Michele Mammini
+/////////////////////////////////////////////////////////////////////
+
+Dbmng.BootstrapItaliaTheme = Dbmng.AbstractTheme.extend({
+  getFieldContainer: function(aField) {
+    var el = document.createElement('div');
+    el.className = 'dbmng_form_row';
+    if (aField.widget!=='checkbox' && aField.widget!=='select') {
+      el.className = 'form-group dbmng_bi_field';
+    }
+    if (aField.widget=='select') {
+      el.className = 'bootstrap-select-wrapper';
+    }
+    el.className = el.className + ' dbmng_form_field_' + aField.field;
+
+    return el;
+  },
+  getLabel: function(aField) {
+    var el=document.createDocumentFragment();
+
+    if (aField.widget!=='checkbox') {
+      el=document.createElement('label');
+      el.className='active';
+      el.setAttribute('for', 'dbmng_' + aField.field);
+
+      // if set assign the label long in form mode
+      var label = aField.label;
+      if( aField.label_long ) {
+        label = aField.label_long;
+      }
+      el.innerHTML=label;
+
+      if( parseInt(aField.nullable) === 0  ) {
+        var sp = document.createElement('span');
+        sp.className='dbmng_required';
+
+        var star=document.createTextNode('*');
+        sp.appendChild(star);
+      }
+    }
+
+    return el;
+  },
+  getInput: function(aField) {
+    var el=document.createElement('input');
+    el.className='form-control';
+
+    if (aField.type=='varchar' || aField.type=='text') {
+      el.setAttribute('type', 'text');
+    }
+    else if(aField.type=='date') {
+      el.setAttribute('type', 'date');
+    }
+
+    if(typeof aField.value !== 'undefined' ) {
+      el.value=aField.value;
+    }
+    if( aField.placeholder ) {
+      el.placeholder = aField.placeholder;
+    }
+
+    else {
+      el.type = "text";
+    }
+
+    //if in the option there is a field_type value it will be override the previous one (if the widget is hidden and the type is int it should be hidden)
+    if(aField.field_type){
+      el.type = aField.field_type;
+    }
+
+    return el;
+  },
+  getCheckbox: function(aField) {
+
+
+
+    var el=document.createElement('div');
+
+    var fc=document.createElement('div');
+    fc.className='form-check';
+    el.appendChild(fc);
+
+
+    var input=document.createElement('input');
+    input.className='real_widget';
+    fc.appendChild(input);
+
+    var label=document.createElement('label');
+    fc.appendChild(label);
+    // this.assignAttributes(input, aField);
+    var label_text = aField.label;
+    if( aField.label_long ) {
+      label_text = aField.label_long;
+    }
+
+    label.innerHTML= label_text;
+
+    input.setAttribute('id', 'dbmng_' + aField.field);
+    label.setAttribute('for', 'dbmng_' + aField.field);
+
+
+    if(! aField.exclude_attribute) {
+      this.assignAttributes(input, aField);
+    }
+    //console.log(aField);
+
+    input.type = "checkbox";
+    if(typeof aField.value !== 'undefined' ) {
+      input.value=aField.value;
+    }
+
+    if(aField.checked) {
+      input.checked = true;
+    }
+
+    //console.log(aField);
+    if( aField.placeholder ) {
+      input.placeholder = aField.label;
+    }
+
+    return el;
+  },
+
+  getSelect: function(aField) {
+    var div=document.createElement('div');
+    if (aField.addButton) {
+      div.className='input-group';
+    }
+
+    // debugger
+    // el.parent
+    var el=document.createElement('select');
+    el.className='form-control aepy-select real_widget';
+    this.assignAttributes(el, aField);
+
+    if (aField.searchable) {
+      el.setAttribute('data-live-search', aField.searchable);
+    }
+    if (aField.searchable_placeholder) {
+      el.setAttribute('data-live-search-placeholder', aField.searchable_placeholder);
+    }
+
+    if(aField.voc_val) {
+      var o=document.createElement('option');
+
+      if( aField.placeholder ) {
+        o.text=aField.label;
+        o.disabled = 'disabled';
+      }
+
+      el.options.add(o);
+
+      jQuery.each(aField.voc_val, function(k,v){
+        if(typeof v !== 'string') {
+          jQuery.each(v, function(key,text){
+            o=document.createElement('option');
+            o.value = key; // v[0];
+            o.text= text; // v[1];
+            if( typeof aField.value !== 'undefined' ) {
+              if( aField.value == key ) {
+                o.selected = true;
+              }
+            }
+          });
+        }
+        el.options.add(o);
+      });
+    }
+
+    div.appendChild(el);
+
+    if (aField.addButton) {
+      var addButtonDiv=document.createElement('div');
+      addButtonDiv.className='input-group-append';
+
+      // backtick non riconosciuto da grunt
+      // var button=`<button style="background-color:#06c;" class="${aField.addButtonClass} btn btn-primary btn-icon">
+      //               <svg class="icon icon-white">
+      //               <use xlink:href="/bootstrap-italia/dist/svg/sprite.svg#it-plus"></use>
+      //               </svg>
+      //             </button>`;
+
+      var button='<button style="background-color:#06c;" class="'+aField.addButtonClass+' btn btn-primary btn-icon">' +
+                    '<svg class="icon icon-white">' +
+                    '<use xlink:href="/bootstrap-italia/dist/svg/sprite.svg#it-plus"></use>' +
+                    '</svg>' +
+                  '</button>';
+
+      var addButton=jQuery(button)[0];
+
+      addButtonDiv.appendChild(addButton);
+
+      div.appendChild(addButtonDiv);
+
+    }
+
+    return div;
+  },
+  setErrorState: function(element, ok, message){
+
+    var par=jQuery(element.widget);
+
+    par.parent().find(".invalid-feedback").remove();
+
+    if(ok){
+      par.removeClass('is-invalid').addClass('is-valid');
+    }
+    else{
+      par.parent().append('<div class="invalid-feedback">'+message+'</div>');
+      par.removeClass('is-valid').addClass('is-invalid');
+    }
+
+  },
+  createFileUploadField: function(elv, label, opt){
+    var el = this._super(elv, label, opt);
+    var btn=jQuery(el).find('.fileinput-button');
+    btn.css('width','');
+    btn.addClass('col-xs-6');
+    // this.addClass(btn,'col-xs-6');
+
+    var prg=jQuery(el).find('.progress');
+    prg.css('width','');
+    prg[0].style.cssText="";
+    prg.wrap('<div class="col-xs-6" style="padding-top: 7px;"></div>');
+    prg.find('.progress-bar')[0].style.cssText="";
+
+    return el;
+  },
+  // assignAttributes: function(el, aField) {
+  //   this._super(el, aField);
+  //   this.addClass(el, 'form-control');
+  // },
+  // alertMessage: function(text) {
+  //   var el = this._super(text);
+  //   this.addClass(el, 'alert alert-block alert-danger');
+  //   return el;
+  // },
+  // getTable: function(opt) {
+  //   var div = this._super(opt);
+  //   //this.addClass(div, 'table-responsive');
+  //   this.addClass(div.firstChild, 'table');
+  //   return div;
+  // },
+  // getButton: function(text, opt) {
+  //   var el = this._super(text, opt);
+  //   this.addClass(el, 'btn btn-default');
+  //   return el;
+  // },
+
+  // getDeleteButton: function(label,btn_icon){
+  //
+  //   var icn = document.createElement('i');
+  //   this.addClass(icn,btn_icon);
+  //   this.addTitle(icn, label);
+  //   return icn;
+  // }
 
 });
 
@@ -3092,7 +3378,9 @@ Dbmng.Framework7Theme = Dbmng.AbstractTheme.extend({
     var el = document.createElement('li');
 
     var div1=document.createElement('div');
-    div1.className="item-content item-input";
+    //TODO diego updated
+    div1.className="item-content item-input ";
+    div1.className = div1.className + ' dbmng_form_field_' + aField.field;
     el.appendChild(div1);
 
     var div2=document.createElement('div');
@@ -3147,6 +3435,7 @@ Dbmng.Framework7Theme = Dbmng.AbstractTheme.extend({
   },
   getCheckbox: function(aField) {
     // var el=document.createElement('input');
+
 
     var el= document.createElement('div');
     el.className='item-after';
@@ -3582,16 +3871,12 @@ Dbmng.AbstractWidget = Class.extend({
 
   setErrorState: function(ok, message){
 
-    var par=jQuery(jQuery(this.widget).parents('.dbmng_form_row')[0]);
-    par.find('span.error_message').remove();
+    //validate the fields and write the error messages if present
+    this.theme.setErrorState(this, ok, message);
 
-    if(ok){
-      par.removeClass('alert-danger').addClass('alert-success');
-    }
-    else{
-      par.append('<span class="error_message">'+message+'</span>');
-      par.removeClass('alert-success').addClass('alert-danger');
-    }
+
+   
+
   },
   isValidCustom:function (){
     return {ok:true};
@@ -3801,7 +4086,10 @@ Dbmng.CheckboxWidget = Dbmng.AbstractWidget.extend({
     if(this.aField.value==1){
       checked=true;
     }
-    return this.theme.getCheckbox({'value':this.aField.value,'checked':checked});
+
+    this.aField.checked=checked;
+    // return this.theme.getCheckbox({'value':this.aField.value,'checked':checked});
+    return this.theme.getCheckbox(this.aField);
   },
 
   getValue: function(){
@@ -3934,6 +4222,8 @@ Dbmng.FileWidget = Dbmng.AbstractWidget.extend({
   },
 
   createField: function(data_val){
+
+
     var self=this;
     // console.log(self);
 
@@ -4027,9 +4317,18 @@ Dbmng.FileWidget = Dbmng.AbstractWidget.extend({
                     info.append(self.theme.alertMessage("Not Completed!!!!"));
                   }
                   else{
-                    console.log(data);
-                    self.addFile(info, weburl_file, file.name);
-                    self.setValue(file.name);
+                    var url=weburl_file;
+                    if (file.relative_folder) {
+                      url=file.relative_folder;
+                    }
+
+                    self.addFile(info, url, file.name);
+
+                    var fileValue=file.name;
+                    if (file.relative_path) {
+                      fileValue=file.relative_path;
+                    }
+                    self.setValue(fileValue);
                   }
                 });
               }
@@ -4064,7 +4363,12 @@ Dbmng.FileWidget = Dbmng.AbstractWidget.extend({
       btn_title = self.aField.remove_title_icon;
     }
 
-    info.append("<a target='_NEW' class='dbmng_fileupload_filelink' href='"+weburl_file+file+"'>"+this.assignFileTypeIcon(file)+" "+file+"</a>&nbsp;");
+    if( self.aField.hide_link ){
+      info.append(this.assignFileTypeIcon(file)+" "+file+" ");
+    }
+    else{
+      info.append("<a target='_NEW' class='dbmng_fileupload_filelink' href='"+weburl_file+file+"'>"+this.assignFileTypeIcon(file)+" "+file+"</a>&nbsp;");
+    }
     var del=this.theme.getDeleteButton(btn_title, btn_icon);
     info.append(del);
     console.log("addFile");console.log(weburl_file);console.log(file);
